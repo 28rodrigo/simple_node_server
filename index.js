89a5d1aa68369ea1df49576a3dde5f34a9ea66dc
28rodrigo <28rodrigopereira@gmail.com>
2022-06-21 15:22:28 +0100

    essential

---
+++ index.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const fs = require('fs');
+
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.get('/', (req, res) => {
+    res.send('GET OUT OF HERE!');
+});
+
+app.post('/',(req,res)=>{
+    var data_json={
+        date:new Date().toISOString().replace(/T/, ' ').replace(/\..+/, ''),
+        email:req.body.email
+    }
+    let data = JSON.stringify(data_json);
+    data=data.concat(',')
+    fs.appendFileSync('email.txt', data);
+    res.send("OK");
+
+});
+
+
+app.listen(port, () => console.log(`Listening on port ${port}!`))
